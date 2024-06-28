48e772c1b7abde020b772264c842d1e873d754a3
gubnota <hvw@yandex.com>
2024-06-28 08:50:19 +0300

    Initial Commit

---
+++ astrodating/astrodatingApp.swift
@@ -0,0 +1,17 @@
+//
+//  astrodatingApp.swift
+//  astrodating
+//
+//  Created by Vladislav Muravyev on 28-06-2024.
+//
+
+import SwiftUI
+
+@main
+struct astrodatingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
